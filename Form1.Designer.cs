a53bba44cd46af8e83b651b32ab49c8f2afd216a
hyc <hyc_email555@163.com>
2019-05-21 16:11:04 +0800

    修改SQLServer数据库的char、varchar类型为nchar、nvarchar的小程序

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labService = new System.Windows.Forms.Label();
+            this.tbServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbAccount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDatabase = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTable = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labService
+            // 
+            this.labService.AutoSize = true;
+            this.labService.Location = new System.Drawing.Point(71, 66);
+            this.labService.Name = "labService";
+            this.labService.Size = new System.Drawing.Size(41, 12);
+            this.labService.TabIndex = 0;
+            this.labService.Text = "服务器";
+            // 
+            // tbServer
+            // 
+            this.tbServer.Location = new System.Drawing.Point(118, 63);
+            this.tbServer.Name = "tbServer";
+            this.tbServer.Size = new System.Drawing.Size(166, 21);
+            this.tbServer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "账号";
+            // 
+            // tbAccount
+            // 
+            this.tbAccount.Location = new System.Drawing.Point(118, 89);
+            this.tbAccount.Name = "tbAccount";
+            this.tbAccount.Size = new System.Drawing.Size(166, 21);
+            this.tbAccount.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "密码";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(118, 117);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(166, 21);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "数据库";
+            // 
+            // tbDatabase
+            // 
+            this.tbDatabase.Location = new System.Drawing.Point(118, 143);
+            this.tbDatabase.Name = "tbDatabase";
+            this.tbDatabase.Size = new System.Drawing.Size(166, 21);
+            this.tbDatabase.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "表";
+            // 
+            // tbTable
+            // 
+            this.tbTable.Location = new System.Drawing.Point(118, 169);
+            this.tbTable.Name = "tbTable";
+            this.tbTable.Size = new System.Drawing.Size(166, 21);
+            this.tbTable.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "修改";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(18, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(369, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "把char、varchar修改成nchar和nvarchar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(290, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "（模糊查询）";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 256);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbTable);
+            this.Controls.Add(this.tbDatabase);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbAccount);
+            this.Controls.Add(this.tbServer);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labService);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labService;
+        private System.Windows.Forms.TextBox tbServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbAccount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDatabase;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTable;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        const string INFPATH = @"changetable.inf";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 点击修改按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string server = tbServer.Text.Trim();
+            string account = tbAccount.Text.Trim();
+            string password = tbPassword.Text.Trim();
+            string database = tbDatabase.Text.Trim();
+            string table = tbTable.Text.Trim();
+            saveInf(server, account, password, database, table);
+            //连接数据库
+            string connStr = @"server=" + server + ";database=" + database + ";user id=" + account + ";pwd=" + password;
+            SqlConnection conn = new SqlConnection(connStr);
+            try
+            {
+                DataTable dt = getSqlTable(conn, table);
+                if (dt.Rows.Count > 0)
+                {
+                    executeSQLFromTable(dt, conn);
+                    MessageBox.Show("修改成功");
+                }
+                else
+                    MessageBox.Show("没有需要修改的列");
+            }
+            catch
+            {
+                MessageBox.Show("发生一个错误");
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// 获取一个存储sql语句的表
+        /// </summary>
+        /// <param name="conn">The connection.</param>
+        /// <param name="tableStr">The table string.</param>
+        /// <returns></returns>
+        private DataTable getSqlTable(SqlConnection conn, string tableStr)
+        {
+            string sql = "SELECT 'alter table ['+d.name+ '] alter column [' + a.name + '] n'" +
+                          "+ b.name + '(' + cast(a.length * 2 as varchar) + ')' as my_sql " +
+                          "FROM syscolumns a " +
+                          "left join systypes b on a.xtype = b.xusertype " +
+                          "inner join sysobjects d on a.id = d.id and d.xtype = 'U' and d.name like '" + tableStr + "' and d.name <> 'dtproperties'" +
+                          "where " +
+                          "b.name in('char', 'varchar') " +
+                          "and " +
+                          "not exists(SELECT 1 FROM sysobjects where xtype = 'PK' and name in ( " +
+                          "SELECT name FROM sysindexes WHERE indid in (" +
+                          "SELECT indid FROM sysindexkeys WHERE id = a.id AND colid = a.colid" +
+                          "))) " +
+                          "order by d.name,a.name";
+            //string sql = "select * from " + tableStr;
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        /// <summary>
+        /// 执行表中的sql语句
+        /// </summary>
+        /// <param name="dt">The dt.</param>
+        /// <param name="conn">The connection.</param>
+        private void executeSQLFromTable(DataTable dt, SqlConnection conn)
+        {
+            conn.Open();
+            foreach(DataRow dr in dt.Rows)
+            {
+                SqlCommand cmd = new SqlCommand(dr[0] as string, conn);
+                cmd.ExecuteNonQuery();
+            }
+            conn.Close();
+        }
+
+        /// <summary>
+        /// 保存输入信息
+        /// </summary>
+        /// <param name="server">The server.</param>
+        /// <param name="account">The account.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="database">The database.</param>
+        /// <param name="table">The table.</param>
+        private void saveInf(string server, string account, string password, string database, string table)
+        {
+            if (!File.Exists(INFPATH))
+                File.Create(INFPATH).Close();
+            using (StreamWriter writer = new StreamWriter(INFPATH))
+            {
+                writer.WriteLine(server);
+                writer.WriteLine(account);
+                writer.WriteLine(password);
+                writer.WriteLine(database);
+                writer.WriteLine(table);
+            }
+        }
+
+        /// <summary>
+        /// 加载窗口时，从文件读取上次输入的信息
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using(StreamReader reader = new StreamReader(INFPATH))
+                {
+                    tbServer.Text = reader.ReadLine();
+                    tbAccount.Text = reader.ReadLine();
+                    tbPassword.Text = reader.ReadLine();
+                    tbDatabase.Text = reader.ReadLine();
+                    tbTable.Text = reader.ReadLine();
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+
+}
